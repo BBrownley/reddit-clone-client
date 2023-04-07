0cef163077c23551b946105008af953d730fe12a
BBrownley <brendobrownley@yahoo.com>
2023-04-07 00:12:25 -0400

    .

--- src/services/utils/baseUrl.js
+++ src/services/utils/baseUrl.js
@@ -1,5 +1,9 @@
 require("dotenv").config();
 
+// const backendURL = "http://localhost:5000"; // local backend
+
 const backendURL = process.env.REACT_APP_BACKEND_URL;
+
+console.log(backendURL);
 
 export default backendURL;
