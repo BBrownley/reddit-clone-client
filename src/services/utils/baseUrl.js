7942623cd28e16b58ad8adac54a9e5bbb0c625ba
BBrownley <brendobrownley@yahoo.com>
2023-04-10 16:20:35 -0400

    updated react backend url

--- src/services/utils/baseUrl.js
+++ src/services/utils/baseUrl.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-// const backendURL = "http://localhost:5000"; // local backend
+//const backendURL = "http://localhost:5000"; // local backend
 
 const backendURL = process.env.REACT_APP_BACKEND_URL;
 
